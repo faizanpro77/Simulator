61b691a10c650f05e2de3193c09d20502b7db2c2
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-05 13:29:08 +0530

    [faizan] modified  flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-count=0
-count1=0
-while [ $count -lt 21 -a $count1 -lt 21 ]
+echo "coin flip 10 times"
+for ((i=1; i<=10; i++))
 do
 if [ $((RANDOM%2)) -eq 0 ]
 then
@@ -14,24 +13,3 @@ fi
 done
 echo "number of time head="$count
 echo "number of time tail="$count1
-
-if [ $count -eq $count1 ]
-then
-echo "match is Tie"
-else
-echo "match is win"
-fi
-
-if [ $count -eq 21 -a $count -ne $count1 ]
-then
-echo "Head win"
-diff=$((count-count1))
-echo "Head win with point="$diff
-elif [ $count1 -eq 21 -a $count -ne $count1 ]
-then
-echo "Tail win"
-diff1=$((count1-count))
-echo "Tail win with point="$diff1
-fi
-
-
